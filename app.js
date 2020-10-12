e7fe2288cffd971a4ca81cbb1ffafbd24f82ec16
Zac Smith <zrs3141592@gmail.com>
2020-10-11 19:15:47 -0600

    express server with socket.io

---
+++ app.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const socketio = require('socket.io')
+const mongoose = require('mongoose')
+require('colors')
+
+const app = express()
+const expressServer = app.listen(process.env.PORT)
+const io = socketio(expressServer)
+
+const connectDB = mongoose.connect(process.env.MONGO_URI, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, () => console.log(`\n** MongoDB is connected`.rainbow))
+
