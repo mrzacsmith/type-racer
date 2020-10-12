5a04c89b3ddd524540389daff311112cc9140536
Zac Smith <zrs3141592@gmail.com>
2020-10-11 20:13:58 -0600

    game schema

---
+++ models/Game.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+
+const PlayerSchema = new mongoose.Schema({
+  currentWordIndex: { type: Number, default: 0 },
+  socketID: { type: String },
+  isPartyLeader: { type: Boolean, default: false },
+  WPM: { type: Number, defautl: -1 },
+  nickName: { type: String }
+})
+
+const GameSchema = new mongoose.Schema({
+  words: [ { type: Sting } ],
+  isOpen: { type: Boolean, default: true },
+  isOver: { type: Boolean, default: false },
+  players: [ PlayerSchema ],
+  startTime: { type: Number }
+})
+
+module.exports = mongoose.model('Game', GameSchema)
