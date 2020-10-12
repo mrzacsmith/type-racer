6650ececf02700eab789d835ae79357e9e026f26
Zac Smith <zrs3141592@gmail.com>
2020-10-11 20:23:50 -0600

    add quotable api

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const app = express()
 const expressServer = app.listen(process.env.PORT)
 const io = socketio(expressServer)
 
+const Game = require('./models/Game.js')
+
 const connectDB = mongoose.connect(process.env.MONGO_URI, {
   useCreateIndex: true,
   useNewUrlParser: true,
